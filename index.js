323bf6d1c5ccb7aae8ed4584acab2e627e7bf5ed
ChuckBTaylor <WLScripps@gmail.com>
2017-10-19 14:14:40 -0400

    Ride the light

---
+++ index.js
@@ -0,0 +1,148 @@
+window.onload = function(){
+  addEventListeners()
+  let lng, lat;
+}
+
+function addEventListeners(){
+  $('#button-location').on('click', () =>{
+    handleButton()
+  })
+
+  $('#location-form').submit(ev => {
+    ev.preventDefault();
+    handleForm()
+  })
+
+  $('#table-holder').mouseover(ev => {
+
+    if (ev.target && ev.target.nodeName === "TD" && ev.target.classList[0] !== "ignore-mouse"){
+      handleMouseover(ev.target)
+    }
+  })
+}
+
+
+function handleButton(){
+  getLocation().then(res => {
+    lat = res[0]
+    lng = res[1]
+    fetchSunInfo(lat, lng)
+  })
+}
+
+function getLocation(){
+  return new Promise ((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(function (position){
+      resolve([position.coords.latitude, position.coords.longitude]);
+    })
+  })
+}
+
+function fetchSunInfo(lat, lng) {
+  fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`)
+  .then(resp => resp.json())
+  .then(sunInfo => {
+
+    getTimeZone(lat, lng).then(timeInfo => {
+      addRow(doCalculations(timeInfo, sunInfo.results), lat, lng)
+    })
+  });
+}
+
+function getTimeZone(lat, lng){
+  return fetch(`https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lng}&timestamp=${Math.floor(Date.now()/1000)}&key=AIzaSyDODC9E1OJs0z6HhK38RuaId8dnePChvWA`)
+  .then(gRes => gRes.json());
+}
+
+function doCalculations(timeInfo, sunInfo) {
+  let rise, set, sMoon, dayLength;
+  if(timeInfo.status !== "ZERO_RESULTS"){
+    rise = getOffSetTime(sunInfo.sunrise, timeInfo)
+    set = getOffSetTime(sunInfo.sunset, timeInfo)
+    sMoon = getOffSetTime(sunInfo.solar_noon, timeInfo)
+    dayLength = sunInfo.day_length
+  }else{
+    rise = sunInfo.sunrise
+    set = sunInfo.sunset
+    sMoon = sunInfo.solar_noon
+    dayLength = sunInfo.day_length
+  }
+  return {rise, set, sMoon, dayLength}
+}
+
+function getOffSetTime(timeString, timeInfo){
+  const reg = new RegExp(/^\d{1,2}/)
+  const offSet = Math.floor((timeInfo.rawOffset + timeInfo.dstOffset)/3600)
+  let offSetHour = (+timeString.match(reg)[0]) + offSet
+  if (offSetHour < 1){
+    offSetHour += 12
+  }else if (offSetHour >= 12){
+    offSetHour -= 12
+  }
+
+  return timeString.replace(reg, offSetHour)
+}
+
+function addRow(sunObj, lat, lng){
+  let table = document.getElementById('sun-table-body')
+
+  if (!table){
+    addTable()
+    table = document.getElementById('sun-table-body')
+  }
+
+  let row = document.createElement('tr')
+  row.innerHTML = `
+    <td class='ignore-mouse'>${Math.round(lat*10000)/10000}</td>
+    <td class='ignore-mouse'>${Math.round(lng*10000)/10000}</td>
+    <td>${sunObj.rise}</td>
+    <td>${sunObj.set}</td>
+    <td>${sunObj.sMoon}</td>
+    <td class='ignore-mouse'>${sunObj.dayLength}</td>`
+  table.prepend(row)
+
+}
+
+function addTable(){
+  const table = document.createElement('table')
+  table.setAttribute('id', 'sun-table')
+  table.innerHTML =       `
+    <tr>
+      <th>Latitude</th>
+      <th>Longitude</th>
+      <th>Sunrise</th>
+      <th>Sunset</th>
+      <th>Sailor Noon</th>
+      <th>Length of Day</th>
+      <tbody id='sun-table-body'></tbody>
+    </tr>`
+  document.getElementById('table-holder').appendChild(table)
+}
+
+function handleMouseover(target){
+  let hour = parseInt(target.textContent.match(/^\d{1,2}/)[0])
+  hour = target.textContent.slice(-2, -1) === "P" ? hour + 12 : hour
+  console.log(hour);
+
+}
+
+function handleForm(){
+  const form = $('#location-form')
+  const lat = santizeUserInput(form[0][0].value)
+  const lng = santizeUserInput(form[0][1].value)
+  if (parseFloat(lat) > 90 || parseFloat(lat) < -90 || parseFloat(lng) > 180 || parseFloat(lng) < -180){
+    return displayError()
+  }
+  fetchSunInfo(lat, lng)
+}
+
+function santizeUserInput(number){
+  if (!number.match(/\./)){
+    number += "."
+  }
+  return number
+}
+
+function displayError(){
+  console.log("Your coordinates suck");
+}
---
+++ sunride.js
@@ -0,0 +1,125 @@
+const sunride = document.getElementById('sunride');
+const ctx = sunride.getContext("2d");
+let c = 0;
+document.getElementById('cycle').addEventListener('click', cycle);
+
+function cycle() {
+  ctx.clearRect(0, 0, sunride.width, sunride.height);
+
+  // if (hour <= 7 && hour > 4) {
+  //   drawDawn();
+  // } else if (hour <= 10 && hour > 7) {
+  //   drawMidMorning();
+  // } else if (hour <= 2 && hour > 10) {
+  //   drawNoon();
+  // } else if (hour <= 6 && hour > 2) {
+  //   drawMidAfternoon();
+  // } else if (hour <= 9 && hour > 6) {
+  //   drawDusk();
+  // } else {
+  //   drawNight();
+  // }
+
+  switch (c) {
+    case 0 : drawDawn(); c++; break;
+    case 1 : drawMidMorning(); c++; break;
+    case 2 : drawNoon(); c++; break;
+    case 3 : drawMidAfternoon(); c++; break;
+    case 4 : drawDusk(); c++; break;
+    case 5 : drawnNight(); c = 0; break;
+  }
+};
+
+function drawDawn() {
+  const grd = ctx.createRadialGradient(25, 300, 20, 25, 300, 35);
+  grd.addColorStop(0, "#e5c352");
+  grd.addColorStop(1, "#404159");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+
+  // ctx.fillStyle = '#404159';
+  // ctx.fillRect(0, 0, sunride.width, sunride.height);
+  // ctx.fill();
+
+  // ctx.fillStyle = '#e5c352';
+  // ctx.beginPath();
+  // ctx.arc(25, 300, 35, 0, 2*Math.PI);
+  // ctx.fill();
+};
+
+function drawMidMorning() {
+  const grd = ctx.createRadialGradient(200, 150, 20, 200, 150, 35);
+  grd.addColorStop(0, "#eacc46");
+  grd.addColorStop(1, "#41607f");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+
+  // ctx.fillStyle = '#41607f';
+  // ctx.fillRect(0, 0, sunride.width, sunride.height);
+  // ctx.fill();
+
+  // ctx.fillStyle = '#eacc46';
+  // ctx.beginPath();
+  // ctx.arc(212, 150, 35, 0, 2*Math.PI);
+  // ctx.fill();
+};
+
+function drawNoon() {
+
+  const grd = ctx.createRadialGradient(400, 100, 20, 400, 100, 37);
+  grd.addColorStop(0, "#efce28");
+  grd.addColorStop(1, "#5493ce");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+
+
+  // ctx.fillStyle = '#5493ce';
+  // ctx.fillRect(0, 0, sunride.width, sunride.height);
+  // ctx.fill();
+
+  // ctx.fillStyle = '#efce28';
+  // ctx.beginPath();
+  // ctx.arc(400, 100, 35, 0, 2*Math.PI);
+  // ctx.fill();
+};
+
+function drawMidAfternoon() {
+  const grd = ctx.createRadialGradient(600, 150, 20, 600, 150, 35);
+  grd.addColorStop(0, "#ddbc37");
+  grd.addColorStop(1, "#4b74c1");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+
+  // ctx.fillStyle = '#4b74c1';
+  // ctx.fillRect(0, 0, sunride.width, sunride.height);
+  // ctx.fill();
+
+  // ctx.fillStyle = '#ddbc37';
+  // ctx.beginPath();
+  // ctx.arc(650, 150, 35, 0, 2*Math.PI);
+  // ctx.fill();
+}
+
+function drawDusk() {
+  const grd = ctx.createRadialGradient(774, 300, 20, 774, 300, 35);
+  grd.addColorStop(0, "#c69f33");
+  grd.addColorStop(1, "#404159");
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+
+  // ctx.fillStyle = '#404159';
+  // ctx.fillRect(0, 0, sunride.width, sunride.height);
+  // ctx.fill();
+
+  // ctx.fillStyle = '#c69f33';
+  // ctx.beginPath();
+  // ctx.arc(774, 300, 35, 0, 2*Math.PI);
+  // ctx.fill();
+}
+
+function drawNight() {
+  ctx.fillStyle = '#191938';
+  ctx.fillRect(0, 0, sunride.width, sunride.height);
+  ctx.fillStyle = '#d1d0cf'
+  for(let i=0; i<)
+}
